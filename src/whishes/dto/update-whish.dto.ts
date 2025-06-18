aa7b15061813ff32323974f8cea0a5d1b0ba07c6
Aleksey <109232395+FlintAB@users.noreply.github.com>
2025-06-18 17:40:56 +0300

    Delete src/whishes directory

--- src/whishes/dto/update-whish.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateWhishDto } from './create-whish.dto';
-
-export class UpdateWhishDto extends PartialType(CreateWhishDto) {}
--- src/whishes/whishes.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { WhishesService } from './whishes.service';
-import { WhishesController } from './whishes.controller';
-
-@Module({
-  controllers: [WhishesController],
-  providers: [WhishesService],
-})
-export class WhishesModule {}
